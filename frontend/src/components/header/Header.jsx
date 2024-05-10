5f33620c71265acf0ff74c59ec908a2bcff06c5f
kidde60 <kgeorgewilliam60@gmail.com>
2024-05-10 23:15:59 +0300

    Update header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="bg">
+      <div className="text">
+        <h1>Order Your Goods Today</h1>
+        <p>
+          SalesPro is your go-to destination for seamless online shopping,
+          offering a wide range of products from top brands at your fingertips.
+          Whether you're looking for fashion, electronics, home decor, or
+          groceries, ShopEase provides a convenient and secure platform to
+          browse, shop, and receive your favorite items with ease.
+        </p>
+        <button>View menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
