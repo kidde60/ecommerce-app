8420b01ff612b5552c6c51717ffa5ead9d7c1503
kidde60 <kgeorgewilliam60@gmail.com>
2024-05-10 23:16:26 +0300

    Update home page

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../header/Header";
+import Menu from "../menu/Menu";
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Menu />
+    </div>
+  );
+};
+
+export default Home;
