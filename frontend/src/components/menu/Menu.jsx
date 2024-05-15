d72049d09878d0943c032c38ca5f56038cb0d0d4
kidde60 <kgeorgewilliam60@gmail.com>
2024-05-15 07:57:45 +0300

    update menu

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -1,45 +1,33 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+
 import "./Menu.css";
-const Menu = () => {
-  const [data, setData] = useState();
-
-  // Effect to fetch data when the component mounts
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        // Make GET request to API
-        const response = await axios.get("https://fakestoreapi.com/products");
-
-        // Set the fetched data to state
-        console.log(response.data);
-        setData(response.data);
-      } catch (error) {
-        // Handle errors
-        console.error("Error fetching data:", error.message);
-      }
-    }
-    console.log(data);
-    // Call fetchData function to initiate data fetching
-    fetchData();
-  }, []); // Empty dependency array to run the effect only once when the component mounts
-
+const Menu = ({ data }) => {
   return (
-    <div className="App">
+    <div className="menu">
       <h1>Explore Our Menu</h1>
+      <p>
+        Sales Pro stands out for its commitment to excellence in every aspect of
+        the shopping experience. With a vast selection of products,
+        user-friendly interface, secure transactions, and attentive customer
+        service, Sales Pro ensures that every shopping journey is convenient,
+        enjoyable, and hassle-free. Whether you're shopping for yourself or
+        searching for the perfect gift, Sales Pro is your trusted partner in
+        online shopping.
+      </p>
       {data ? (
         <div className="list">
           {data.map((item) => (
             <div key={item.id} className="list-item">
               <img src={item.image} alt={item.title} />
-
-              <h2>${item.price}</h2>
+              <h1>{item.title}</h1>
+              <h2>$ {item.price}</h2>
             </div>
           ))}
         </div>
       ) : (
         <p>Loading...</p>
       )}
+      <hr />
     </div>
   );
 };
